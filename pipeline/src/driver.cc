039b47e8a902ca9da72d3b9decea9f7cad987b13
Skyler Ruiter <skyler.ruiter1@gmail.com>
2025-02-06 14:46:38 -0500

    small cmake changes/refactor

---
+++ pipeline/src/driver.cc
@@ -0,0 +1,9 @@
+// get all modules compiled at least w/ low level C++ api
+
+// one compiled work on C and C++ api interface w/ team
+
+// python interface once we're happy with C/C++ api (ctypes?) (SWIG?)
+
+// libpressio integration? ask robert/song/PIs
+
+// sz3 integration? add in these modules -- fit this to SZ3 or SZ3 to this?
